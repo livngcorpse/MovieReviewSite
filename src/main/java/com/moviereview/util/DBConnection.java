c5a13726465e9e6aea06bffd01c6640b1df3bf11
Vyse <arifmohammad2612@gmail.com>
2025-10-23 10:49:02 +0530

    Second commit

--- src/main/java/com/moviereview/util/DBConnection.java
+++ src/main/java/com/moviereview/util/DBConnection.java
@@ -5,13 +5,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/movie_review_db";
+    private static final String URL = "jdbc:mariadb://localhost:3306/movie_review_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "277353"; // Change this
+    private static final String PASSWORD = "277353"; // Change if needed
     
     static {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName("org.mariadb.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
